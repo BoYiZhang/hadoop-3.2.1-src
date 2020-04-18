34b5d28cbff7c0081b8483c72379a55549449d68
BoYiZhang <zhangboyi_mx@163.com>
2020-04-18 14:16:42 +0800

    hadoop rpc 样例

---
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/rpc/Client.java
@@ -0,0 +1,21 @@
+package org.apache.rpc;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.ipc.RPC;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+/**
+ * 访问RPC服务
+ */
+public class Client {
+  public static void main(String[] args) throws IOException {
+    //1. 拿到RPC协议
+    ClicentNameNodeProtocol proxy = RPC.getProxy(ClicentNameNodeProtocol.class, 1L,
+        new InetSocketAddress("localhost", 7777), new Configuration());
+    //2. 发送请求
+    String metaData = proxy.getMetaData("/meta");
+    //3. 打印元数据
+    System.out.println(metaData);
+  }
+}
